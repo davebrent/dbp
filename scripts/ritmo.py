2ba8ceda8416a3f80bd942e347404459030fd41c
Dave <dave@paperlessreceipts.co.uk>
2014-02-10 22:22:06 +0000

    Fixing transport beat changes, renaming classes

--- scripts/ritmo.py
+++ scripts/ritmo.py
@@ -51,8 +51,8 @@ class MidiDevice(object):
     Keeps track of note_off events if a duration is supplied.
 
     """
-    def __init__(self, device):
-        self.device = device
+    def __init__(self, connection):
+        self.connection = connection
         self.note_off_heap = Heap(key=lambda note: note["duration"])
 
     def note_on(self, note=60, velocity=64, channel=0, duration=None):
@@ -60,7 +60,7 @@ class MidiDevice(object):
                             "channel={}, duration={}".format(note, velocity,
                                                              channel,
                                                              duration)))
-        self.device.send_message([0x90 + channel, note, velocity])
+        self.connection.send_message([0x90 + channel, note, velocity])
         if duration:
             self.note_off_heap.push({"note": note,
                                      "channel": channel,
@@ -69,7 +69,7 @@ class MidiDevice(object):
     def note_off(self, note=60, channel=0):
         self._log("debug", "note_off: note={}, channel={}".format(
             note, channel))
-        self.device.send_message([0x80 + channel, note, 0])
+        self.connection.send_message([0x80 + channel, note, 0])
 
     def tick(self):
         while True:
@@ -88,14 +88,16 @@ class MidiDevice(object):
     def control(self, control=0, value=0, channel=0):
         self._log("debug", "control: control={}, value={}, channel={}".format(
             control, value, channel))
-        self.device.send_message([0x80 + channel, int(control), int(value)])
+        self.connection.send_message([0x80 + channel,
+                                      int(control), int(value)])
 
     def _log(self, level, msg):
         getattr(logger, level)("MidiDevice.{}".format(msg))
 
 
 @contextlib.contextmanager
-def connect(port_name=None, midi_class=rtmidi.MidiOut):
+def connect(port_name=None, connection_class=rtmidi.MidiOut,
+            device_class=MidiDevice):
     """Connect the rtmidi device to an available port.
 
     If no port_name is specified but midi ports are available, the device will
@@ -104,9 +106,9 @@ def connect(port_name=None, midi_class=rtmidi.MidiOut):
     Otherwise an Exception is raised.
 
     """
-    device = midi_class()
-    class_name = midi_class.__name__
-    available_ports = device.get_ports()
+    connection = connection_class()
+    class_name = connection_class.__name__
+    available_ports = connection.get_ports()
     port_number = 0
 
     if not available_ports and not port_name:
@@ -114,14 +116,14 @@ def connect(port_name=None, midi_class=rtmidi.MidiOut):
     elif not available_ports and port_name:
         logger.info("Connecting on a virtual midi port {}.").format(port_name)
         port_number = None
-        device.open_virtual_port(port_name)
+        connection.open_virtual_port(port_name)
     elif port_name:
         found = False
         for port, port_name in enumerate(available_ports):
             if port_name != port_name:
                 continue
             port_number = port
-            device.open_port(port)
+            connection.open_port(port)
             found = True
             logger.info("{} ({}), on port {}.".format(class_name, port_name,
                                                       port))
@@ -131,58 +133,62 @@ def connect(port_name=None, midi_class=rtmidi.MidiOut):
     else:
         port_number = 0
         port_name = available_ports[port_number]
-        device.open_port(port_number)
+        connection.open_port(port_number)
         logger.info("{} ({}), on port 0.".format(class_name, port_name))
 
-    midi_device = MidiDevice(device)
+    device = device_class(connection)
 
     try:
-        yield midi_device
+        yield device
     finally:
-        midi_device.all_notes_off()
-        device.close_port()
+        device.all_notes_off()
+        connection.close_port()
         logger.info(("{} closed ({}), on port {}."
                      .format(class_name, port_name, port_number)))
 
 
-class TimeCode(object):
+class Transport(object):
 
     def __init__(self):
-        """Keep track of the position of the midi clock in a musical way."""
+        """Keep track of the position of the clock in a musical way."""
         self.reset()
 
     def reset(self):
         self.ticks = 0
         self.bars = 0
-        self.bar_changed = False
+        self.bar_changed = True
+        self._bar_off_tick = 1
         self.beats = 0
-        self.beat_changed = False
-        self.time = time.time()
+        self.beat_changed = True
+        self._beat_off_tick = 1
 
     def tick(self, ppqn, numerator, denominator):
-        self.time = time.time()
         self.ticks += 1
 
         ticks_per_beat = (ppqn * 4) / denominator
 
         if self.ticks % ticks_per_beat == 0 and not self.beat_changed:
             self.beats += 1
+            self._beat_off_tick = self.ticks + 1
             self.beat_changed = True
-        elif self.ticks % ticks_per_beat == 1 and self.beat_changed:
+        elif self._beat_off_tick == self.ticks and self.beat_changed:
             self.beat_changed = False
+            self._beat_off_tick = -1
 
         if self.beats % numerator == 0 and not self.bar_changed:
             self.bars += 1
+            self._bar_off_tick = self.ticks + 1
             self.bar_changed = True
-        elif self.beats % numerator == 1 and self.bar_changed:
+        elif self._bar_off_tick == self.ticks and self.bar_changed:
             self.bar_changed = False
+            self._bar_off_tick = -1
 
 
 def get_ticksize_from_bpm(bpm, ppqn=24):
     return 60.0 / (float(bpm) * float(ppqn))
 
 
-class MidiClock(object):
+class Clock(object):
 
     def __init__(self, bpm=120, numerator=4, denominator=4, ppqn=24,
                  wait=0.0002):
@@ -202,20 +208,23 @@ class MidiClock(object):
         self.wait = wait
 
     def run(self, device, func):
-        timecode = TimeCode()
+        transport = Transport()
         start = current = time.time()
 
-        logger.info(("MidiClock.run: bpm={}, wait={}, ticksize={}"
+        logger.info(("Clock.run: bpm={}, wait={}, ticksize={}"
                      .format(self.bpm, self.wait, self.ticksize)))
 
         while True:
             if current - start >= self.ticksize:
                 self.ticksize = get_ticksize_from_bpm(self.bpm, ppqn=self.ppqn)
-                # Midi device is ticked first to process note off events,
-                # timecode is ticked last to update the latest position.
+                # Device is ticked first to process note off events
                 device.tick()
-                func(device, timecode, self)
-                timecode.tick(self.ppqn, self.numerator, self.denominator)
+                # Allow the callable to end the loop by returning False
+                result = func(device, transport, self)
+                if result is False:
+                    break
+                # Timecode is ticked last incase the callable updates the clock
+                transport.tick(self.ppqn, self.numerator, self.denominator)
                 start += self.ticksize
             current = time.time()
             time.sleep(self.wait)
@@ -234,12 +243,12 @@ if __name__ == "__main__":
     logger.setLevel(logging.INFO)
     logger.addHandler(logging.StreamHandler(sys.stdout))
 
-    def main(device, timecode, clock):
-        if timecode.beat_changed:
+    def main(device, transport, clock):
+        if transport.beat_changed:
             device.note_on(note=54, duration=clock.ticksize)
 
     with connect(port_name="IAC Driver Bus 1") as device:
-        clock = MidiClock(bpm=120)
+        clock = Clock(bpm=120)
         try:
             clock.run(device, main)
         except KeyboardInterrupt:
